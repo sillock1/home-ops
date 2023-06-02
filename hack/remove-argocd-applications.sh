ae0420d0e14656dbbf89ced0c36386bb48f67fe4
sillock1 <jprest97@gmail.com>
2023-06-02 16:03:30 +0100

    Added registration config to dendrite

--- hack/remove-argocd-applications.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-kubectl get applications.argoproj.io -n argocd -o name | xargs -I {} kubectl patch {} --type=json -p='[{"op": "remove", "path": "/metadata/finalizers"}]' -n argocd
