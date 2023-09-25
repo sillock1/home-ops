cee37d6796982e111c1aefce2a97cabf817e1041
sillock1 <jprest97@gmail.com>
2023-09-25 22:30:12 +0100

    chore: updated talos and K8s versions

--- provision/talos/apply-clusterconfig.sh
+++ provision/talos/apply-clusterconfig.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # Deploy the configuration to the nodes
-talosctl apply-config -n 10.1.7.20 -f ./clusterconfig/cluster-production-polaris.pill.ac.yaml
-talosctl apply-config -n 10.1.7.21 -f ./clusterconfig/cluster-production-rigel.pill.ac.yaml
-talosctl apply-config -n 10.1.7.22 -f ./clusterconfig/cluster-production-sirius.pill.ac.yaml
-talosctl apply-config -n 10.1.7.30 -f ./clusterconfig/cluster-production-mercury.pill.ac.yaml
-talosctl apply-config -n 10.1.7.31 -f ./clusterconfig/cluster-production-venus.pill.ac.yaml
-talosctl apply-config -n 10.1.7.32 -f ./clusterconfig/cluster-production-earth.pill.ac.yaml
+talosctl apply-config -n 10.1.7.20 -f ./clusterconfig/cluster-production-polaris.pill.ac.yaml --insecure
+talosctl apply-config -n 10.1.7.21 -f ./clusterconfig/cluster-production-rigel.pill.ac.yaml --insecure
+talosctl apply-config -n 10.1.7.22 -f ./clusterconfig/cluster-production-sirius.pill.ac.yaml --insecure
+talosctl apply-config -n 10.1.7.30 -f ./clusterconfig/cluster-production-mercury.pill.ac.yaml --insecure
+talosctl apply-config -n 10.1.7.31 -f ./clusterconfig/cluster-production-venus.pill.ac.yaml --insecure
+talosctl apply-config -n 10.1.7.32 -f ./clusterconfig/cluster-production-earth.pill.ac.yaml --insecure
