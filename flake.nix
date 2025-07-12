9719b0c4914343db3eab251e463375babd7fd1d6
sillock <mail@sillock.com>
2025-07-12 12:50:21 +0100

    chore: use mise and flakes for dev environment

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "Home ops flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    # Ancient version in nixpkgs
+    zarf = {
+      url = "https://github.com/zarf-dev/zarf/releases/download/v0.58.0/zarf_v0.58.0_Linux_amd64";
+      flake = false;
+    };
+  };
+
+  outputs = { nixpkgs, ... } @ inputs:
+    let
+      pkgs = nixpkgs.legacyPackages."x86_64-linux";
+    in
+    {
+      devShells."x86_64-linux".default = with pkgs; mkShell {
+        packages = [
+          helm
+          helmfile
+          kustomize
+          talosctl
+          sops
+          kind
+          cilium-cli
+          hubble
+          fluxcd
+          chart-testing
+          opentofu
+          minijinja
+          go-task
+          k9s
+          kubectl
+          restic
+          kubectl
+        ];
+
+      };
+  };
+}
